f46153adc91c4dd4d94d60d7b8f6c1dfdd1b3588
Shiv sagar <bcashivsagar@gmail.com>
2026-03-31 11:14:11 +0530

    fix: switch to HashRouter for foolproof static deployments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import useAuthStore from './stores/authStore';
